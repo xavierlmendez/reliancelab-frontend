e0728e6eaa39af8ff68ad49c23ba7cc0cfe8e0a2
xaviermendez <xluis10.2@gmail.com>
2026-03-27 18:25:42 -0500

    fix for env var usage instead of string

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,11 @@ import App from './App.tsx'
 import './index.css'
 import { PreSessionProvider } from './contexts/PreSessionContext.tsx'
 
+const API_ENDPOINT = import.meta.env.VITE_BACKEND_URL ?? 'http://api.reliance-hci.com'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FetchProvider endpoint="http://api.reliance-hci.com">
+    <FetchProvider endpoint={API_ENDPOINT}>
       <ToastProvider>
         <RoutingProvider>
           <PreSessionProvider>
